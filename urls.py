5ebc2b50f3735a43e7a08bc19532a4fb6dff7e2c
AramKarimi <aram.karimi@gu.se>
2023-03-01 12:28:50 +0100

    remove auth url

--- urls.py
+++ urls.py
@@ -6,5 +6,5 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
