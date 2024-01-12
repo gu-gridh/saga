a5456b4f81c092793c07f6048c315edf1ef49ac5
AramKarimi <aram.karimi@gu.se>
2024-01-12 11:35:01 +0100

    update admin page with new fields

--- admin.py
+++ admin.py
@@ -10,6 +10,8 @@ from django.contrib.gis import admin
 @admin.register(Archive)
 class SagaArchiveAdmin(admin.ModelAdmin):
     list_display = get_fields(Archive, exclude=DEFAULT_EXCLUDE+["id"])
+    search_fields = ['name', 'name2', 'title', 'parent_name']
+    autocomplete_fields = ['parent_name']
 
 @admin.register(ArtForm)
 class SagaArtFormAdmin(admin.ModelAdmin):
@@ -22,14 +24,19 @@ class SagaRoleAdmin(admin.ModelAdmin):
 @admin.register(Publisher)
 class SagaPublisherAdmin(admin.ModelAdmin):
     list_display = get_fields(Publisher) 
+    search_fields = ['title']
 
 @admin.register(Series)
 class SagaSeriesAdmin(admin.ModelAdmin):
     list_display = get_fields(Series) 
+    search_fields = ['title', 'publisher_title']
+    autocomplete_fields = ['publisher_title']
 
 @admin.register(Volume)
 class SagaVolumeAdmin(admin.ModelAdmin):
-    list_display = get_fields(Volume) 
+    list_display = get_fields(Volume)
+    search_fields = ['name', 'title', 'series_name']
+    autocomplete_fields = ['series_name'] 
 
 @admin.register(Place)
 class SagaPlaceAdmin(admin.ModelAdmin):
@@ -40,21 +47,25 @@ class RelIllustrEditionAdmin(admin.TabularInline):
     model = RelIllustrEdition
     extra = 1
 
+
 class RelPersonIllustrationAdmin(admin.TabularInline):
     model = RelPersonIllustration
     extra = 1
+
 
 class RelPersonTextAdmin(admin.TabularInline):
     model = RelPersonText
     extra = 1
 
+
 class RelTextEditionAdmin(admin.TabularInline):
     model = RelTextEdition
     extra = 1
    
-
 class SagaEditionAdmin(admin.ModelAdmin):
     list_display = get_fields(Edition, exclude=DEFAULT_EXCLUDE+["id"]) 
+    search_fields = ['title', 'volume_name']
+    autocomplete_fields = ['volume_name', 'publisher_id', 'printer_id', 'archive_id']
     inlines = [
             RelIllustrEditionAdmin,
             RelTextEditionAdmin,
@@ -62,6 +73,7 @@ class SagaEditionAdmin(admin.ModelAdmin):
 
 class SagaIllustrationAdmin(admin.ModelAdmin):
     list_display = get_fields(Illustration, exclude=DEFAULT_EXCLUDE+["id"])
+    autocomplete_fields = ['archive_name']
     inlines =  [
         RelIllustrEditionAdmin,
         RelPersonIllustrationAdmin,
@@ -71,13 +83,19 @@ class SagaPersonAdmin(admin.ModelAdmin):
     list_display = get_fields(Person, exclude=DEFAULT_EXCLUDE+["id"])  
     inlines = [
             RelPersonIllustrationAdmin, 
-            RelPersonTextAdmin,
+            RelPersonTextAdmin, 
             ]
+    
+@admin.register(Work)   
 class SagaWorkAdmin(admin.ModelAdmin):
     list_display = get_fields(Work, exclude=DEFAULT_EXCLUDE+["id"])
+    search_fields = ['title']
+
 
 class SagaTextAdmin(admin.ModelAdmin):
     list_display = get_fields(SagaText, exclude=DEFAULT_EXCLUDE+["id"])  
+    search_fields = ['archive_name']
+    autocomplete_fields = ['archive_name', 'work_id']
     inlines = [
             RelPersonTextAdmin,
             RelTextEditionAdmin
